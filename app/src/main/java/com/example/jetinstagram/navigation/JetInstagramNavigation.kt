757b3b31d3c207096abaf06dbeb096711158b8c0
Darfune <harolep@gmail.com>
2022-08-16 23:23:47 +0300

    added notification popUp composable

--- app/src/main/java/com/example/jetinstagram/navigation/JetInstagramNavigation.kt
+++ app/src/main/java/com/example/jetinstagram/navigation/JetInstagramNavigation.kt
@@ -7,11 +7,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetinstagram.firebase.FirebaseHandlerViewModel
 import com.example.jetinstagram.screens.authetication.SignupScreen
+import com.example.jetinstagram.widgets.NotificationMessage
 
 @Composable
 fun JetInstagramNavigation() {
     val navController = rememberNavController()
     val viewModel = hiltViewModel<FirebaseHandlerViewModel>()
+    NotificationMessage(viewModel = viewModel)
     NavHost(navController = navController, startDestination = JetInstagramScreens.SignupScreen.name) {
         composable(JetInstagramScreens.SignupScreen.name) {
             SignupScreen(navController = navController, viewModel = viewModel)
