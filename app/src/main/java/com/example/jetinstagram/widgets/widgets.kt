be382089b86afbb41b005b386ada92f654b1aeb1
Darfune <harolep@gmail.com>
2022-08-24 22:10:58 +0300

    file with all the widgets

---
+++ app/src/main/java/com/example/jetinstagram/widgets/widgets.kt
@@ -0,0 +1,121 @@
+package com.example.jetinstagram.widgets
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.AsyncImagePainter
+import coil.compose.ImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.jetinstagram.R
+import com.example.jetinstagram.firebase.FirebaseHandlerViewModel
+import com.example.jetinstagram.navigation.JetInstagramScreens
+
+
+//Notifications widget
+@Composable
+fun NotificationMessage(viewModel: FirebaseHandlerViewModel) {
+    val notificationState = viewModel.popupNotification.value
+    val notificationMessage = notificationState?.getContentOrNull()
+    if (notificationMessage != null) {
+        Toast.makeText(LocalContext.current, notificationMessage, Toast.LENGTH_LONG).show()
+    }
+}
+
+//Navigation widgets
+fun navigateTo(navController: NavController, destination: JetInstagramScreens) {
+    navController.navigate(destination.name) {
+        popUpTo(destination.name)
+        launchSingleTop = true
+    }
+}
+
+@Composable
+fun CheckSignIn(navController: NavController, viewModel: FirebaseHandlerViewModel) {
+    val alreadyLoggedIn = remember {
+        mutableStateOf(false)
+    }
+    val signIn = viewModel.signedIn.value
+    if (signIn && !alreadyLoggedIn.value) {
+        alreadyLoggedIn.value = true
+        navController.navigate(JetInstagramScreens.FeedScreen.name) {
+            popUpTo(0)
+        }
+    }
+
+}
+
+//Common Progress Spinner
+@Composable
+fun CommonProgressSpinner() {
+    Row(
+        modifier = Modifier
+            .alpha(0.5f)
+            .background(Color.LightGray)
+            .clickable(enabled = false) { }
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+//Show Image widget
+@Composable
+fun CommonImage(
+    data: String?,
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    val painter = rememberAsyncImagePainter(data)
+    Image(
+        painter = painter, contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
+    if (painter.state is AsyncImagePainter.State.Loading) {
+        CommonProgressSpinner()
+    }
+}
+
+@Composable
+fun UserImageCard(
+    userImage: String?,
+    modifier: Modifier = Modifier
+        .padding(8.dp)
+        .size(64.dp),
+) {
+    Card(
+        shape = CircleShape,
+        modifier = modifier
+    ) {
+        if (userImage.isNullOrEmpty()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user_default),
+                contentDescription = "Default user icon",
+                colorFilter = ColorFilter.tint(Color.Gray)
+            )
+        } else {
+            CommonImage(data = userImage)
+        }
+    }
+}
