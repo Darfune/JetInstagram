0b8cb4e506fd319608d355e09c2d0f24508632a6
Darfune <harolep@gmail.com>
2022-08-24 22:11:32 +0300

    Added top image

--- app/src/main/java/com/example/jetinstagram/screens/myposts/MyPostsScreen.kt
+++ app/src/main/java/com/example/jetinstagram/screens/myposts/MyPostsScreen.kt
@@ -1,21 +1,30 @@
 package com.example.jetinstagram.screens.myposts
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.jetinstagram.R
 import com.example.jetinstagram.firebase.FirebaseHandlerViewModel
 import com.example.jetinstagram.menu.BottomNavigationItem
 import com.example.jetinstagram.menu.BottomNavigationMenu
+import com.example.jetinstagram.widgets.UserImageCard
 
 @Composable
 fun MyPostScreen(navController: NavController, viewModel: FirebaseHandlerViewModel) {
@@ -84,5 +93,34 @@ fun MyPostScreen(navController: NavController, viewModel: FirebaseHandlerViewMod
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
-
+    Box(modifier = Modifier
+        .padding(16.dp)
+        .clickable { onClick.invoke() }) {
+        UserImageCard(
+            userImage = imageUrl,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(
+                width = 2.dp,
+                color = Color.White
+            ),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = "add icon",
+                modifier = Modifier
+                    .background(Color.Blue)
+            )
+        }
+    }
 }
+
+
